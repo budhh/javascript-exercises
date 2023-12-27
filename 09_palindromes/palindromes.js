693c68d7a5ce8632a07992619367883c66732a88
Abdullah <47166459+budhh@users.noreply.github.com>
2023-12-27 13:46:28 -0800

    finished palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+	filteredString = string.replace(/[^\w\']|_/g, '');
+	filteredString = filteredString.toLowerCase();
+	arr = filteredString.split('');
+	arrlen = Math.floor(arr.length / 2);
+	acc = arr.length - 1;
+	for (let index = 0; index < arrlen; index++) {
+		if (arr[index] !== arr[acc]) {
+			return false;
+		}
+		acc -= 1;
+	}
+	return true;
 };
 
 // Do not edit below this line
