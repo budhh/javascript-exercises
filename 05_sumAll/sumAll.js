9105eb71708d747b4743330168b1c2b50c593bea
Abdullah <47166459+budhh@users.noreply.github.com>
2023-12-27 12:32:46 -0800

    finished sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+	if (
+		start < 0 ||
+		end < 0 ||
+		start instanceof String ||
+		end instanceof String ||
+		typeof start === 'string' ||
+		typeof end === 'string' ||
+		isNaN(start) ||
+		isNaN(end)
+	) {
+		return 'ERROR';
+	}
+	smallNum = start;
+	bigNum = end;
+	if (start > end) {
+		smallNum = end;
+		bigNum = start;
+	}
+	sum = 0;
+	for (let index = smallNum; index <= bigNum; index++) {
+		sum += index;
+	}
 
+	return sum;
 };
 
 // Do not edit below this line
