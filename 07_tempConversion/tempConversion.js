cd5ff001f9551dad42239e524f2bf06a11e62805
Abdullah <47166459+budhh@users.noreply.github.com>
2023-12-27 12:46:14 -0800

    done temperatur conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+	return round(((temp - 32) * 5) / 9, 1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+	return round((temp * 9) / 5 + 32, 1);
 };
+
+function round(value, precision) {
+	var multiplier = Math.pow(10, precision || 0);
+	return Math.round(value * multiplier) / multiplier;
+}
 
 // Do not edit below this line
 module.exports = {
-  convertToCelsius,
-  convertToFahrenheit
+	convertToCelsius,
+	convertToFahrenheit,
 };
