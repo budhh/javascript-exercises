7437c92e99a64577f32af45f150e78f22c23076c
Abdullah <47166459+budhh@users.noreply.github.com>
2023-12-27 12:12:29 -0800

    asdasda

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
 const removeFromArray = function (array, ...options) {
-	filteredArray = array.filter((x) => !options.includes(x));
+	filteredArray = array;
+	for (let index = 0; index < filteredArray.length; index++) {
+		const x = filteredArray[index];
+		if (options.includes(x)) {
+			filteredArray.splice(index, 1);
+			index--;
+		}
+	}
 	return filteredArray;
 };
 
