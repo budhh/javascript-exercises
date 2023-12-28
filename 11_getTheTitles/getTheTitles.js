1037c6d56254580df082a516587bfd7622441a14
Abdullah Haroon <47166459+budhh@users.noreply.github.com>
2023-12-28 10:45:21 -0800

    finished 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (books) {
+	var titles = [];
+	x = 0;
+	books.forEach((element) => {
+		titles[x] = element.title;
+		x++;
+	});
+	return titles;
 };
 
 // Do not edit below this line
