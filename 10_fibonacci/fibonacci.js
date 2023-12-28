5eff10ba306a77c7470913aff40402bdfb2a82f6
Abdullah Haroon <47166459+budhh@users.noreply.github.com>
2023-12-28 10:35:37 -0800

    finished fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function (num, memo) {
+	var n = parseInt(num);
+	// n = num;
 
+	memo = memo || {};
+
+	if (memo[n]) return memo[n];
+
+	if (n < 0) {
+		return 'OOPS';
+	}
+
+	if (n == 0) {
+		return 0;
+	}
+
+	if (n == 1) {
+		return 1;
+	}
+	return (memo[n] = fibonacci(n - 1, memo) + fibonacci(n - 2, memo));
 };
 
 // Do not edit below this line
