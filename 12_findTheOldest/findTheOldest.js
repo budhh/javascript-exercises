01ef3c0d1e139a23b506d412eff65efd1d17a3ba
Abdullah Haroon <47166459+budhh@users.noreply.github.com>
2023-12-28 11:36:50 -0800

    finished oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (array) {
+	var max = array[0];
+	var maxAge = 0;
+	var currYear = 2023;
+	for (let index = 0; index < array.length; index++) {
+		const curr = array[index];
+		if (curr.yearOfDeath !== undefined) {
+			currAge = curr.yearOfDeath - curr.yearOfBirth;
+		} else {
+			currAge = currYear - curr.yearOfBirth;
+		}
+		if (currAge >= maxAge) {
+			maxAge = currAge;
+			max = curr;
+		}
+	}
+	return max;
 };
 
 // Do not edit below this line
